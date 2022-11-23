b09d4943b425cfdbe88b16d5985b3fad02fb1efe
Maxim Romanov <romanov.maxim@gmail.com>
2022-11-23 15:02:23 +0100

    website update

--- deploy_on_github.sh
+++ deploy_on_github.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd -- "$(dirname "$BASH_SOURCE")"
+rm -rf docs
 cd theme_stack
 mv public ../docs
 
@@ -7,11 +8,3 @@ cd ..
 git add .
 git commit -m "website update"
 git push
-
-#mkdir -p "$HOME/Library/Application Support/org.kde.syntax-highlighting/syntax"
-#cp ./SCHEME_FILES/OpenITImARkdown.xml "$HOME/Library/Application Support/org.kde.syntax-highlighting/syntax"
-#cp ./SCHEME_FILES/OpenITImARkdown_EIS1600.xml "$HOME/Library/Application Support/org.kde.syntax-highlighting/syntax"
-#echo "- OpenITImARkdownSH (main flavor) highlighting scheme for Kate has been installed."
-#echo "- OpenITImARkdownSH (EIS1600 Flavor) highlighting scheme for Kate has been installed."
-#echo "Close Kate and (re-)open it for the changes to take effect."
-
