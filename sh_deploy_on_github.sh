d5a5a1dd860f0488578a5158fe718b6924a743b4
Maxim Romanov <romanov.maxim@gmail.com>
2023-10-20 19:44:12 +0200

    website automatic update

--- sh_deploy_on_github.sh
+++ sh_deploy_on_github.sh
@@ -16,34 +16,23 @@ echo "- the old DOCS folder has been deleted."
 mv $pathToFreshWebsite $pathToWebsite
 echo "- the DOCS folder has been replaced with newly generated content."
 
+
 # CLEANING
-for folder in $(find $pathToWebsite -type d); do
-    if [[ $folder == *"/ar/"* ]]; then
-        for suffix in "_DE" "_EN" "_FA" "_RU"; do
-            [ -d "${folder}${suffix}" ] && rm -rf "${folder}${suffix}"
-        done
-    elif [[ $folder == *"/de/"* ]]; then
-        for suffix in "_AR" "_EN" "_FA" "_RU"; do
-            [ -d "${folder}${suffix}" ] && rm -rf "${folder}${suffix}"
-        done
-    elif [[ $folder == *"/en/"* ]]; then
-        for suffix in "_AR" "_DE" "_FA" "_RU"; do
-            [ -d "${folder}${suffix}" ] && rm -rf "${folder}${suffix}"
-        done
-    elif [[ $folder == *"/fa/"* ]]; then
-        for suffix in "_AR" "_DE" "_EN" "_RU"; do
-            [ -d "${folder}${suffix}" ] && rm -rf "${folder}${suffix}"
-        done
-    elif [[ $folder == *"/ru/"* ]]; then
-        for suffix in "_AR" "_DE" "_EN" "_FA"; do
-            [ -d "${folder}${suffix}" ] && rm -rf "${folder}${suffix}"
-        done
-    else
-        for suffix in "_AR" "_DE" "_EN" "_FA" "_RU"; do
-            [ -d "${folder}${suffix}" ] && rm -rf "${folder}${suffix}"
-        done
+for folder in $(find $pathToWebsite -type d -name "*_AR" -o -name "*_DE" -o -name "*_EN" -o -name "*_FA" -o -name "*_RU"); do
+    if [[ $folder == *"/ar/"* && ! $folder == *"_AR" ]]; then
+        rm -rf "$folder"
+    elif [[ $folder == *"/de/"* && ! $folder == *"_DE" ]]; then
+        rm -rf "$folder"
+    elif [[ $folder == *"/en/"* && ! $folder == *"_EN" ]]; then
+        rm -rf "$folder"
+    elif [[ $folder == *"/fa/"* && ! $folder == *"_FA" ]]; then
+        rm -rf "$folder"
+    elif [[ $folder == *"/ru/"* && ! $folder == *"_RU" ]]; then
+        rm -rf "$folder"
     fi
 done
+
+
 
 echo "- the new DOCS folder has been cleaned."
 
